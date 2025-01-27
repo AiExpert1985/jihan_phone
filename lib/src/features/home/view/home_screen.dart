7f9208a946e696396ea7b8ddcba1aa75ee189b06
Shaheen <AiExpert2025@gmail.com>
2025-01-27 13:49:32 +0300

    ...

--- lib/src/features/home/view/home_screen.dart
+++ lib/src/features/home/view/home_screen.dart
@@ -21,6 +21,7 @@ class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _HomeScreenState createState() => _HomeScreenState();
 }
 
